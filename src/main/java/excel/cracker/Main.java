9b3a4f2ab6ced8c4e389be04c7ec7d2fa76c925d
Zorglube <zorglube@broncotoxique.com>
2021-12-07 14:34:07 +0100

    Toolchain JDK and Native-Image, write password in file if found.

--- src/main/java/excel/cracker/Main.java
+++ src/main/java/excel/cracker/Main.java
@@ -2,10 +2,15 @@ package excel.cracker;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.security.GeneralSecurityException;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
@@ -13,6 +18,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.IntStream;
 
 import org.apache.poi.poifs.crypt.Decryptor;
@@ -22,6 +28,8 @@ import org.apache.poi.poifs.filesystem.POIFSFileSystem;
 public class Main {
 
 	// Parameters
+	private static Instant chronoStart = Instant.now();
+	private static AtomicLong test = new AtomicLong(0);
 	private static boolean debug = false;
 	private static int minPasswordLength;
 	private static int maxPasswordLength;
@@ -31,12 +39,12 @@ public class Main {
 	static {
 		// Character set for cracking
 		final var characters = new ArrayList<Character>();
-//		// Numbers
-//		IntStream.range(48, 58).forEach(i -> characters.add((char) i));
-//		// Lower case
-//		IntStream.range(65, 91).forEach(i -> characters.add((char) i));
-//		// Upper case
-//		IntStream.range(97, 123).forEach(i -> characters.add((char) i));
+		// // Numbers
+		// IntStream.range(48, 58).forEach(i -> characters.add((char) i));
+		// // Lower case
+		// IntStream.range(65, 91).forEach(i -> characters.add((char) i));
+		// // Upper case
+		// IntStream.range(97, 123).forEach(i -> characters.add((char) i));
 		IntStream.range(33, 126).forEach(i -> characters.add((char) i));
 		charSet = characters.toArray(new Character[characters.size()]);
 	}
@@ -68,7 +76,7 @@ public class Main {
 		Main.executeRunnableDesiredTimes(Main.threadCount - 1, threadPoolExecutor, consumer);
 
 		final var result = Main.crackPassword(threadPoolExecutor, cf);
-		System.out.println(String.format("Password found: {}", result));
+		System.out.println(String.format("\nPassword found: %s", result));
 	}
 
 	private static String crackPassword(final ExecutorService service, final CompletableFuture<String> cf) {
@@ -76,11 +84,14 @@ public class Main {
 		try {
 			final var start = Instant.now();
 			result = cf.get();
+			Files.write(Paths.get("password_found.txt"), Arrays.asList(result), StandardCharsets.UTF_8,
+					StandardOpenOption.APPEND);
 			final var finish = Instant.now();
 			final var timeElapsed = Duration.between(start, finish).toMillis();
-			System.out.println(String.format("Total time elapsed: %s", timeElapsed));
-		} catch (InterruptedException | ExecutionException e) {
-			System.err.println("crackPassword");
+			System.out.println(String.format("\nPassword found: %s", result));
+			System.out.println(String.format("\nTotal time elapsed: %s", timeElapsed));
+		} catch (InterruptedException | ExecutionException | IOException e) {
+			System.err.println("\ncrackPassword");
 			e.printStackTrace();
 		} finally {
 			service.shutdownNow();
@@ -100,7 +111,7 @@ public class Main {
 				try {
 					Main.generatePasswordsForDesiredLength(charSet, i, "", charSet.length, passwordQueue);
 				} catch (final InterruptedException e) {
-					System.err.println("passwordProvider : generatePasswordsForDesiredLength : InterruptedException");
+					System.err.println("\npasswordProvider : generatePasswordsForDesiredLength : InterruptedException");
 					e.printStackTrace();
 				}
 			}
@@ -120,24 +131,27 @@ public class Main {
 
 	private static void offeing(final String pass, final BlockingQueue<String> passwordQueue)
 			throws InterruptedException {
-		System.out.println(String.format("Offering : %s", pass));
 		passwordQueue.offer(pass, 12, TimeUnit.HOURS);
 	}
 
-	private static Runnable passwordCracker(final CompletableFuture<String> cf,
-			final BlockingQueue<String> passwordQueue, final Decryptor excelDecryptor) {
+	private static Runnable passwordCracker(final CompletableFuture<String> cf, final BlockingQueue<String> passwordQueue,
+			final Decryptor excelDecryptor) {
 		return () -> {
 			var password = "";
 			try {
 				while (!Main.finished && !excelDecryptor.verifyPassword(password)) {
 					password = passwordQueue.take();
-//					if (debug)
-//						System.err.printf("Testing password: {}", password);
+					System.out.print(
+							String.format("Offering : %s, %010d pw, %10d pw/s, since %d min\r", password, test.incrementAndGet(),
+									Math.floorMod(test.get(), Duration.between(chronoStart, Instant.now()).toSeconds() + 1),
+									Duration.between(chronoStart, Instant.now()).toMinutes()));
+					// if (debug)
+					// System.err.printf("Testing password: {}", password);
 				}
 				cf.complete(password);
 				Main.finished = true;
 			} catch (InterruptedException | GeneralSecurityException e) {
-				System.err.println("passwordCracker");
+				System.err.println("\npasswordCracker");
 				e.printStackTrace();
 			}
 		};
